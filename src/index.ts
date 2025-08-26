f09c37d83598a412569da5d1adffc673d59ff00b
hienhhcc <thachdau16t@gmail.com>
2025-08-26 19:35:21 +0700

    Create simple expressjs application

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use("/", (req, res) => {
+  res.send("hello");
+});
+
+const startServer = async () => {
+  app.listen(PORT, () => {
+    console.log("App in running on PORT: " + PORT);
+  });
+};
+
+startServer();
